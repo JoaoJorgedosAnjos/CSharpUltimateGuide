5ae51ca029aef633e749468f46309aabd3390431
João Jorge <joaojorgedosanjos@gmail.com>
2025-12-26 00:19:36 -0300

    teste ssh key

---
+++ Assignment03FeetToCentimeters/Program.cs
@@ -0,0 +1,10 @@
+﻿const double InchesInCentimeters = 2.54;
+const double FeetToCentimeter = 12 * InchesInCentimeters;
+
+double ValueFeet = 5 * FeetToCentimeter;
+double ValueInches = 7 * InchesInCentimeters;
+
+var Sum = ValueInches + ValueFeet;
+
+Console.WriteLine($"{Sum} cm");
+Console.WriteLine("teste");
